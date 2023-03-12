53b559045f3ea69ad05e1eac48efb11e4f4a0713
Jacobmartin22 <Martinjeb32@gmail.com>
2023-03-12 18:30:34 -0500

    new file:   .css 	modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -1,56 +1,91 @@
-const { Button } = require("bootstrap");
-
 var app = new function() {
-    this.el=document.getElementById('Tasks')
-    this.tasks=[]
-
-    this.fetchAll; {
-        var data="";
-
-        if(this.tasks.length>0){
-            for(i=0; i<this.tasks.lengths; i++){
-                data+='<tr>';
-                data+='<td>'+(i+1)+'. '+this.task[i]+'</td>';
-                data+='<td><Button onclick="app.Edit('+i+')'
-                class='btn btn-warning">Edit</button></td> ';
-                data+='<td><Button onclick="app.Delete('+i+')'
-                class='btn btn-danger">Delete</button></td> ';
-                data+='</tr>'
-            };
+    this.el = document.getElementById('tasks');
+  
+    this.tasks = [];
+  
+    
+    
+    this.FetchAll = function() {
+      var data = '';
+  
+      if (this.tasks.length > 0) {
+        for (i = 0; i < this.tasks.length; i++) {
+          data += '<tr>';
+          data += '<td>'+(i+1)+". " + this.tasks[i] + '</td>';
+          data += '<td><button onclick="app.Edit(' + i + ')"  class="btn btn-warning">Edit</button></td>';
+          data += '<td><button onclick="app.Delete(' + i + ')"  class="btn btn-danger">Delete</button></td>';
+          data += '</tr>';
         }
-        this.count(this.tasks.length);
-        return this.el.innerHTML = data;
+      }
+  
+      this.Count(this.tasks.length);
+      return this.el.innerHTML = data;
     };
-
-    this.Add= function(){
-        el= document.getElementById('add-hotel');
-        var task=el.value;
-        if(task){
-            this.tasks.push(task.trim());
-            el.value='';
-            this.FetchAll
+  
+    this.Add = function () {
+      el = document.getElementById('add-todo');
+      // Get the value
+      var task = el.value;
+  
+      if (task) {
+        // Add the new value
+        this.tasks.push(task.trim());
+        // Reset input value
+        el.value = '';
+        // Dislay the new list
+        this.FetchAll();
+      }
+    };
+  
+    this.Edit = function (item) {
+      var el = document.getElementById('edit-todo');
+      // Display value in the field
+      el.value = this.tasks[item];
+      // Display fields
+      document.getElementById('edit-box').style.display = 'block';
+      self = this;
+  
+      document.getElementById('save-edit').onsubmit = function() {
+        // Get value
+        var task = el.value;
+  
+        if (task) {
+          // Edit value
+          self.tasks.splice(item, 1, task.trim());
+          // Display the new list
+          self.FetchAll();
+          // Hide fields
+          CloseInput();
         }
-
+      }
     };
-
-    this.Edit = function(item) {
-
+  
+    this.Delete = function (item) {
+      // Delete the current row
+      this.tasks.splice(item, 1);
+      // Display the new list
+      this.FetchAll();
     };
-
-    this.Delete = function(item) {
-
-    };
-
+  
     this.Count = function(data) {
-
+      var el   = document.getElementById('counter');
+      var name = 'Tasks';
+  
+      if (data) {
+          if(data ==1){
+              name = 'Task'
+          }
+        el.innerHTML = data + ' ' + name ;
+      } 
+      else {
+        el.innerHTML = 'No ' + name;
+      }
     };
- 
-    }
-
-
-
-app.FetchAll();
-
-function CloseInput() {
-    document.getElementById('edit-box').style.display= 'none';
-}
+    
+  }
+  
+  app.FetchAll();
+  
+  function CloseInput() {
+    document.getElementById('edit-box').style.display = 'none';
+  }
